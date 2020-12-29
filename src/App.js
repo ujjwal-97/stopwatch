8e91ffed5a04792df6e1c92ccd6fae91df8d72ed
Ujjwal Gautam <ujm2099@gmail.com>
2020-12-29 13:22:41 +0530

    29dec init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React,{useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [timer, setTimer]= useState(0);
+  const [isactive, setIsActive]= useState(false);
+  const [ispaused, setIsPaused]= useState(false);
+  const countRef= useRef();
+
+  const handleStart= () =>{
+    setIsActive(true)
+    setIsPaused(true)
+    countRef.current =  setInterval(()=>{
+      setTimer((timer)=> timer+1)
+    },1000)
+  }
+  const handlePause= () =>{
+    clearInterval(countRef.current)
+    setIsPaused(false)
+  }
+  const handleResume= () =>{
+    setIsPaused(true)
+    countRef.current = setInterval(() =>{
+      setTimer((timer) => timer+1)
+    },1000)
+  }
+  const handleReset= () =>{
+    clearInterval(countRef.current)
+    setIsActive(false)
+    setIsPaused(false)
+    setTimer(0)
+  }
+  const formatTime = () =>{9
+    const getSeconds=`0${(timer %60)}`.slice(-2)
+    const minutes=`${Math.floor(timer / 60)}`
+    const getMinutes=`0${minutes % 60}`.slice(-2)
+    const getHours =`0${Math.floor(timer/3600)}`.slice(-2)
+    return `${getHours}: ${getMinutes}: ${getSeconds}`
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>StopWatch with REACT</h2>
+      <div className="stopwatch-card">
+        <p>{formatTime()}</p>
+        <div className="buttons">
+          { !isactive && !ispaused?
+          <button onClick={handleStart}>Start</button>
+          :( ispaused?
+          <button onClick={handlePause}>Pause</button> :
+          <button onClick={handleResume}>Resume</button> 
+          )}
+          <button onClick={handleReset} disabled={!isactive}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
